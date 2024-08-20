610839f7fcea98c1c2ff6262bb301354ac9579be
Harsimran Singh <111746632+Harsimran726@users.noreply.github.com>
2024-08-20 18:12:15 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -9,10 +9,9 @@ source my_env/bin/activate
 # Install dependencies
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-python3 -m pip install -r requirements.txt
 
 # Activate virtual environment (if used)
-source my_env/Scripts/activate  # Windows
+# Windows
 # source my_env/bin/activate     # macOS/Linux
 
 cd "$PROJECT_DIR"
